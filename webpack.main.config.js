eccac002a9f0843d14c715d4b9296bdfbc84811e
clyons793 <christopher_lyons@comcast.com>
2022-10-28 09:18:28 -0400

    adding interactive functionality

---
+++ webpack.main.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    /**
+     * This is the main entry point for your application, it's the first file
+     * that runs in the main process.
+     */
+    entry: './src/index.ts',
+    // Put your normal webpack config below here
+    module: {
+        rules: require('./webpack.rules')
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json']
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, './src/splash.html'),
+                    to: path.resolve(__dirname, './.webpack/main/')
+                },
+                {
+                    from: path.resolve(__dirname, './src/splash.css'),
+                    to: path.resolve(__dirname, './.webpack/main/')
+                },
+                {
+                    from: path.resolve(__dirname, './src/fe/assets'),
+                    to: path.resolve(__dirname, `./.webpack/renderer/Asset/`)
+                }
+            ]
+        })
+    ],
+    optimization: {
+        // Typeorm depends on entity class name to create table alias
+        // if none were defined before, and since webpack minimizes all class name
+        // on production it causes typeorm to mess up every database query.
+        // Therefore turning webpack minification off is a must.
+        // REFERENCE: https://github.com/typeorm/typeorm/issues/4266#issuecomment-505139399
+        minimize: false
+    }
+};
---
+++ webpack.renderer.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const rules = require('./webpack.rules');
+const plugins = require('./webpack.plugins');
+
+rules.push({
+    test: /\.s[ac]ss$/i,
+    use: [
+        { loader: 'style-loader' },
+        { loader: 'css-loader' },
+        { loader: 'sass-loader' }
+    ]
+});
+
+module.exports = {
+    module: {
+        rules
+    },
+    plugins: plugins,
+    resolve: {
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
+    }
+};
