c8d0b5574fc3a534c83b5f30ce9bf6c9af56fff3
clyons793 <christopher_lyons@comcast.com>
2022-10-30 07:39:57 -0400

    Extracting signing variables

---
+++ forge.config.js
@@ -0,0 +1,75 @@
+module.exports = {
+    "makers": [
+        {
+            "name": "@electron-forge/maker-squirrel",
+            "config": {
+                "name": "ops-credential-sharing",
+                "exe": "ops-credential-sharing.exe",
+                "setupExe": "ops-credential-sharing.exe"
+            }
+        },
+        {
+            "name": "@electron-forge/maker-zip",
+            "platforms": [
+                "darwin"
+            ]
+        },
+        {
+            "name": "@electron-forge/maker-deb",
+            "config": {}
+        },
+        {
+            "name": "@electron-forge/maker-rpm",
+            "config": {}
+        }
+    ],
+    "packagerConfig": {
+        "executableName": "ops-credential-sharing",
+        "name": "ops-credential-sharing",
+        "icon": "./src/assets/icons/ops-credential-sharing.ico",
+        "asar": true,
+        "asarUnpack": [
+            "node_modules/@electron/remote/main.js"
+        ],
+        "osxSign": {
+            "identity": "Developer ID Application: Chris Lyons (66SW9S36Q2)",
+            "hardened-runtime": true,
+            "gatekeeper-assess": false,
+            "entitlements": "entitlements.mac.plist",
+            "entitlements-inherit": "entitlements.mac.plist",
+            "signature-flags": "library"
+        },
+        "osxNotarize": {
+            "appleId": process.env.appleid,
+            "appleIdPassword": process.env.password
+        }
+    },
+    "publisher": {
+        "name": "@electron-forge/publisher-github",
+        "config": {
+            "repository": {
+                "owner": "dotcomrow",
+                "name": "ops-credential-sharing"
+            },
+            "authToken": "{GITHUB_TOKEN}"
+        }
+    },
+    "plugins": [
+        {
+            "name": "@electron-forge/plugin-webpack",
+            "config": {
+                "mainConfig": "./webpack.main.config.js",
+                "renderer": {
+                    "config": "./webpack.renderer.config.js",
+                    "entryPoints": [
+                        {
+                            "html": "./src/index.html",
+                            "js": "./src/renderer.ts",
+                            "name": "main_window"
+                        }
+                    ]
+                }
+            }
+        }
+    ]
+}
